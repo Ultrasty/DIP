560ea9b33ec923483384a538fd9aae5cadcf16f0
sty <1455670697@qq.com>
2020-11-02 09:35:32 +0800

    双线性内插

--- main2a.m
+++ main2a.m
@@ -1,12 +1,9 @@
 img = imread('../data2/CT_1.jpg');
 img_modified = uint8(zeros(1024));
-for i = 1:256
-    for j = 1:256
-        for m=1:4
-            for n=1:4
-                img_modified(4*i-m+1,4*j-n+1)=img(i,j);
-            end
-        end
+for i = 1:1024
+    for j = 1:1024
+        % ×î½üÁÚÄÚ²å
+        img_modified(i,j)=img(int32((i+1)/4),int32((j+1)/4));
     end
 end
 imshow(img_modified)
---
+++ main2b.m
@@ -0,0 +1,24 @@
+for i = 1 :1000
+    % Ë«ÏßÐÔÄÚ²å
+    str=['../data2/CT_',num2str(i),'.jpg'];
+    img = imread(str);
+    img_modified = uint8(zeros(1024));
+    the4=1023/254;
+    for x = 1:1024
+        for y = 1:1024
+            thex=(x-1)/the4+1;
+            they=(y-1)/the4+1;
+            x1=floor(thex);
+            x2=floor(thex+1);
+            y1=floor(they);
+            y2=floor(they+1);
+            fR1=(x2-thex)*img(x1,y1)+(thex-x1)*img(x2,y1);
+            fR2=(x2-thex)*img(x1,y2)+(thex-x1)*img(x2,y2);
+            fP=(y2-they)*fR1+(they-y1)*fR2;
+            fP=uint8(fP);
+            img_modified(x,y)=fP;
+        end
+    end
+    str=['../modified images/problem 2b/CT_',num2str(i),'.jpg'];
+    imwrite(img_modified,str);
+end
